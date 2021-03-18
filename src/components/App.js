d32e7fb485c76be4886de84e84bf91cd3c91f951
Gabriel Smith <Gabe@Gabes-Laptop.home>
2021-03-18 16:33:16 -0400

    add: Header component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../style/App.scss';
+import '../style/utilities.scss'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
